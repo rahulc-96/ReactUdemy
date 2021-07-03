8ae482a52927aa65efe65911cc7df9814eb8ef0a
ZEUS <3crahulrao@gmail.com>
2021-07-03 14:32:40 +0530

    adding email input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,34 +3,52 @@ import React, {useState} from "react";
 const SimpleInput = (props) => {
   const [nameTouched, setNameTouched] = useState(false);
   const [enteredName, setEnteredName] = useState("");
-  const nameIsValid = enteredName.trim() !== "";
-  const renderError = nameTouched && !nameIsValid;
-  const formIsValid = nameIsValid;
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [enteredMail, setEnteredMail] = useState("");
+  const nameIsValid = enteredName.trim() !== ""
+  const emailIsValid = enteredMail.trim() !== "" && enteredMail.trim().includes('@');
+  const renderNameError = nameTouched && !nameIsValid;
+  const renderEmailError = emailTouched && !emailIsValid; 
+  const formIsValid = nameIsValid && emailIsValid;
 
   console.log(formIsValid);
-  
+
   const onChangeHandler = (event) => {
+    if(event.target.id === 'name')
     setEnteredName(event.target.value);
+    if(event.target.id === 'email')
+    setEnteredMail(event.target.value);
   }
 
   const onSubmitHandler = (event) => {
          event.preventDefault();
-         console.log(enteredName);
          setEnteredName('');
          setNameTouched(false);
+         setEmailTouched(false);
+         setEnteredMail('')
   }
 
   const onBlurHandler = (event) => {
-      setNameTouched(true);
+    if(event.target.id === 'name')
+      setNameTouched(true)
+    if(event.target.id === 'email')
+      setEmailTouched(true)
   }
 
   return (
     <form onSubmit = {onSubmitHandler}>
-      <div className= {`${!renderError ? 'form-control' : 'form-control invalid' }`}>
+      <div className= {`${!renderNameError ? 'form-control' : 'form-control invalid' }`}>
         <label htmlFor='name'>Your Name</label>
         <input value = {enteredName} onBlur = {onBlurHandler} onChange= {onChangeHandler} type='text' id='name' />
          {
-           renderError && <p className = 'error-text' > Name must not be empty</p>
+           renderNameError && <p className = 'error-text' > Name must not be empty</p>
+         }
+      </div>
+      <div className= {`${!renderEmailError ? 'form-control' : 'form-control invalid' }`}>
+        <label htmlFor='email'>Your Email</label>
+        <input value = {enteredMail} onBlur = {onBlurHandler} onChange= {onChangeHandler} type='text' id='email' />
+         {
+           renderEmailError && <p className = 'error-text' > Enter Valid Email</p>
          }
       </div>
       <div className="form-actions">
